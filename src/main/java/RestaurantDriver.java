b60f5c49227563c5428a7a2176c65a42017906db
Katie Cleland <106635778+kcleland0818@users.noreply.github.com>
2022-09-09 14:21:37 -0600

    Modify lab slightly (#1)

---
+++ src/main/java/RestaurantDriver.java
@@ -0,0 +1,13 @@
+public class RestaurantDriver {
+    public static void main(String[] args) {
+        // Your code here
+    }
+
+    public static void printMenu() {
+        System.out.println("Welcome to the Restaurant! What would you like to do?");
+        System.out.println("0. Exit");
+        System.out.println("1. Check in");
+        System.out.println("2. Check out");
+        System.out.println();
+    }
+}
